dd315ac2b816a940f88a0e983c258a4b2e8d89c8
Szimhead <sz.zmijewski24@gmail.com>
2026-01-30 07:23:23 +0100

    change createBrowserRouter to createHashRouter to fix direct subpage links

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
-  createBrowserRouter,
+  createHashRouter ,
   RouterProvider,
 } from "react-router-dom";
 import { AccessGate } from "@/components/AccessGate";
@@ -47,7 +47,7 @@ const App = () => {
   //   );
   // }
 
-  const router = createBrowserRouter([
+  const router = createHashRouter([
   { path: "/", element:<Index /> },
   { path: "/chart", element:<Chart /> },
   { path: "/library", element:<Library /> },
